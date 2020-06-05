1e2570ed15e25752d3eb711e3a29fa76499f8278
dhate103 <64420319+dhate103@users.noreply.github.com>
2020-06-05 11:32:36 +0900

    역순으로 재배열함.

--- Game programming/Game programming.cpp
+++ Game programming/Game programming.cpp
@@ -246,11 +246,25 @@ void OmokBoard() {//일회성 함수
 #include <iostream>
 using namespace std;
 int main() {
-	int num = 10;
-	int* pointer1 = &num;//pointer1에 num의 주소를 저장한다.
-	int* pointer2 = pointer1;//pointer2 에 pointer1(num의 주소)을 저장한다.
-	(*pointer1)++;//pointer1(10)을 역참조한 값을 1 증가 시킨다.->11
-	(*pointer2)++;//pointer2(==pointer1(11))를 역참조한 값을 1 증가 시킨다.->12
-	cout << num;
-	asdf
+	int arr[7] = { 1,4,3,2,5,7,6 };
+	int* front = &arr[0];
+	int* back = &arr[6];
+
+	for (int i = 0; i < 3; i++) 
+	{
+		int num = *(front+i);//num에 3번 돌려서 1,4,3 순으로 저장됨 - 이거 나중에 뒤에 역순으로 붙이기
+		*(front + i) = *(back - i);//뒷숫자들 마련해놈. 그러니깐 앞 숫자구하기 이제 - 6 7 5 2 5 7 6 됨
+		//그리고 num의 처음값은 1 두번째는 4, 세번째는 3
+		//6 7 5 2 까지 구했으니		3 4 1 로 만들면 된다.
+		*(back - i) = num;
+	}
+
+
+	for (int i = 0; i < 7; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
 }
+
+
